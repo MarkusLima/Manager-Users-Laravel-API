4fdf37da7f7b5a945ee75212f0fbf45ab108c5a8
Markus Lima <markus.lima@fretatech.com.br>
2021-10-30 13:02:32 -0300

    create migrations

--- README.md
+++ README.md
@@ -4,7 +4,18 @@
 ### Create projetc
 30/01/2021
 
-- composer create-project --prefer-dist laravel/laravel blog "6.*"
+```composer create-project --prefer-dist laravel/laravel blog "6.*"
+```
+
+### Create migrates
+php artisan make:migration create_users_table
+php artisan make:migration create_permissions_table
+php artisan make:migration create_permission_users_table
+php artisan make:migration create_addresses_table
+
+![Alt text](./docs/data_base.PNG?raw=true "Database")
+
+
 
 
 
---
+++ database/migrations/2021_10_30_150709_create_permission_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->index('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unsignedBigInteger('permission_id');
+            $table->index('permission_id');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_users');
+    }
+}
